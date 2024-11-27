ca27f669673d8949c452ef637dbbede1b26acaf7
Tinalexman <taiwoluwatobilobafestus@gmail.com>
2024-11-27 11:35:33 +0100

    lskflsk

--- lib/api/cheffy.dart
+++ lib/api/cheffy.dart
@@ -1,9 +1,8 @@
 import 'package:eexily/components/cheffy_message.dart';
 import 'base.dart';
 
-// const String openAIKey =
-//     "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";
-const String openAIKey = "";
+const String openAIKey =
+    "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";
 
 
 Future<EexilyResponse> sendMessageToCheffy(List<CheffyMessage> messages) async {
